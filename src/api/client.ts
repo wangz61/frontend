e5df178fb9cd8e4415a0e0da778c729a68f49a13
wangz61 <18582378131@163.com>
2025-02-23 16:51:21 -0500

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: import.meta.env.API_URL || 'http://localhost:4000',
+    // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
+    baseURL: 'https://backend-v9ow.onrender.com',
 
 });
 
